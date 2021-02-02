9f92af905c792b91ecb0772b226103bcfda07115
Ryan Fritz <rfinky@hotmail.com>
2021-02-01 22:20:56 -0500

    the start instead of hello world

---
+++ cards5/cards5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cards5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("the start");
+        }
+    }
+}
