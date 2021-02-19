e9aef7632437911f40e9c516b342309cfeddcc20
Ryan Fritz <ryanfritzc@gmail.com>
2021-02-19 17:50:04 -0500

    inital commit (added files)

---
+++ cards5main/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cards5main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
