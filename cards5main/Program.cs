e4e9afa24ef6f171204069309be9c6069701e27d
Ryan Fritz <ryanfritzc@gmail.com>
2021-02-23 16:38:39 -0500

    swallowed my pride and sought help for structure in coding. learned more about enumerables and their uses and implemented it in deck structure. rewrote the shuffle method and created 2 classes

--- cards5main/Program.cs
+++ cards5main/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
 
 namespace cards5main
 {
@@ -6,33 +9,12 @@ namespace cards5main
     {
         static void Main(string[] args)
         {
-
-            int[] cards = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,
-                           26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51};
-            
-            Console.WriteLine("original");
-            TesterM(cards);
-
-            Console.WriteLine("shuffled");
-            Shuffle(cards, 52);
-            TesterM(cards);
-            
-
             Console.WriteLine("Lets play some 5 card draw!" +
                               "\nThe rules are simple: Who ever has the best hand wins, you can exchange up to 3 cards in order to better your hand." +
                               "\nYou can raise or fold on the intial hand and after the card exchange, here are your inital 50 coins, Good Luck!");
         }
-        public static void Shuffle(int[] card, int n)
-        {
-            Random rng = new Random();
-            for (int i = 0; i < n; i++)
-            {
-                int s = i + rng.Next(52 - i);
-                int temp = card[s];
-                card[s] = card[i];
-                card[i] = temp;
-            }
-        }
+
+        
         public static void TesterM(int []card)
         {
             int tester = 0;
@@ -50,6 +32,75 @@ namespace cards5main
                 }
             }
             Console.WriteLine("\n");
+        }
+    }
+    public class Card
+    {
+        public enum Suits
+        {
+            Spades,
+            Hearts,
+            Clubs,
+            Diamonds
+        }
+        public enum Values
+        {
+            Two = 2,
+            Three = 3,
+            Four = 4, 
+            Five = 5,
+            Six = 6,
+            Seven = 7,
+            Eight = 8,
+            Nine = 9,
+            Ten = 10,
+            Jack = 11,
+            Queen = 12, 
+            King = 13,
+            Ace = 14,
+        }
+        public Suits getSuits { get; set; }
+        public Values getValues { get; set; }
+    }
+    public class Deck
+    {
+        public const int CARDNUM = 52;
+        private Card[] deck;
+
+        public Deck()
+        {
+            deck = new Card[CARDNUM];
+        }
+        public Card[] getDeck { get; { return deck; } }
+
+        public void createDeck()
+        {
+            int index = 0;
+            foreach(Card.Suits s in Enum.GetValues(typeof(Card.Suits)))
+            {
+                foreach(Card.Values v in Enum.GetValues(typeof(Card.Values)))
+                {
+                    deck[index] = new Card { getSuits = s, getValues = v };
+                    index++;
+                }
+            }
+            Shuffle();
+        }
+        public void Shuffle()
+        {
+            Random rng = new Random();
+            Card temp;
+            for (int shuffle = 0; shuffle < 10; shuffle++)
+            {
+                for ( int i = 0; i < CARDNUM; i++)
+                {
+                    int swapCard = rng.Next(13);
+                    temp = deck[i];
+                    deck[i] = deck[swapCard];
+                    deck[swapCard] = temp;
+
+                }
+            }
         }
     }
 }
